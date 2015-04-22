f8c10287c4eb2bfdc2218e321c71d205dbaa9f41
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2015-04-22 20:10:07 +0200

    Setting up Parse push notifications. Subscribing user to a dummy channel.

--- app/src/main/java/com/imaginarysolutions/givelife/MainActivityFragment.java
+++ app/src/main/java/com/imaginarysolutions/givelife/MainActivityFragment.java
@@ -2,9 +2,14 @@ package com.imaginarysolutions.givelife;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.parse.ParseException;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 
 /**
@@ -13,6 +18,25 @@ import android.view.ViewGroup;
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        boolean isAppFirstStart = true;
+        if (isAppFirstStart) {
+            String userBloodTypeChannel = "AB-positive";
+            // When user enters his blood type, we subscribe them to that channel.
+            ParsePush.subscribeInBackground(userBloodTypeChannel, new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e != null) {
+                        Log.e("Channel subscription", e.getMessage());
+                    }
+                }
+            });
+        }
     }
 
     @Override
