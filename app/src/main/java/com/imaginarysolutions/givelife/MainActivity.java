d693429d9f27f70e8e8c3cf71a719c95ecc2b6a3
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2015-04-28 18:41:02 +0200

    Using Toolbar as ActionBar in MainActivity and extending the AppCompatActivity

--- app/src/main/java/com/imaginarysolutions/givelife/MainActivity.java
+++ app/src/main/java/com/imaginarysolutions/givelife/MainActivity.java
@@ -1,23 +1,27 @@
 package com.imaginarysolutions.givelife;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.parse.ParseAnalytics;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
